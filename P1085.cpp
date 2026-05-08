57664aa5f45c849c5a659b138174f1a3c3376c18
HMMMMMy <16246106+hmmmmmy@user.noreply.gitee.com>
2026-05-08 20:32:30 +0800

    rename project

---
+++ P1085.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int day = 0;
+    int max = 8;//不能写在循环里 因为每次比较完max都会又变成8
+    for(int i = 1;i<=7;i++){
+        int t1,t2;
+        cin>>t1>>t2;
+        //int max = 8;错误！
+        int t = t1+t2;
+
+
+        if (t>max) {
+            max = t;
+            day = i;//必须写在循环里 把最大的天数记录下来
+
+
+        }
+
+    }
+    cout<<day<<endl;
+    return 0;
+}
